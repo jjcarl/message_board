69fd9953d22f0db725736aa49b17a8469c61b864
Jonathon Carlson <jon@jc2dev.com>
2016-07-21 20:12:08 -0600

    updated requirements to current version of Django

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from main.models import Message, Comment, Tag
 
-# Create your tests here.
+
+class MessageTestCase(TestCase):
+
+    # message tests:
+    # Test create, image upload, favorite message, pin, post type
+    def setup(self):
+        pass
+
+    def test_message_create(self):
+        pass
